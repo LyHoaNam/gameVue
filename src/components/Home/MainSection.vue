e2f609de6982825cff3ce7bca23a22e3c5e52d23
lyhoanam <lyhoanam@gmail.com>
2019-11-16 20:14:34 +0700

    base code

---
+++ src/components/Home/MainSection.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>abc</h3>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello world </h1>
+    <Main />
+  </div>
+</template>
+<script>
+import Main from 'components/Home/MainSection';
+export default { 
+  components: {
+    Main
+  }
+}
+</script>
